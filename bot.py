a66f27a6a0bad07c6523f55e110b849ac96e4317
hpynme <trulove551@gmail.com>
2025-06-26 21:25:59 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ import os
 import json
 from datetime import datetime
 
-TOKEN = ("7675019572:AAG4QU1CQ5anaDh4cKXl7F_7O6CfIsSi-I4")  # এটা আলাদা বটের টোকেন হবে
-bot = telebot.TeleBot(TOKEN, parse_mode="MarkdownV2")
+TOKEN = ("7558624142:AAE3iyfWVVxIvN8suZ7dthYvbxKwCVTvq-s") 
 
 STATS_FILE = "stats.json"
 
